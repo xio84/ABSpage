541c29fdbce8f0d5adb7318a4011aa36d9f850ec
Oksidian Tafly <oksitafly@gmail.com>
2021-06-18 15:44:04 +0800

    maps

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
+    // The location of ABS
     const abs = { lat: -3.4292559690354936, lng: 114.80950728465643 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       center: abs,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at ABS
     const marker = new google.maps.Marker({
       position: abs,
       map: map,
